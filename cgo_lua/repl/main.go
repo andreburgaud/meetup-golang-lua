8f5e536732bbd68684b3c3b5a738918206e67d47
Andre <andre.burgaud@gmail.com>
2020-04-15 21:29:45 -0500

    Extracted core of the loop to function

--- cgo_lua/repl/main.go
+++ cgo_lua/repl/main.go
@@ -13,16 +13,16 @@ package main
 import "C"
 
 import (
-    "bufio"
-    "fmt"
-    "io"
-    "os"
-    "strings"
-    "unsafe"
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+	"unsafe"
 )
 
 func help() {
-    msg:= `.exit   Exit the repl
+	msg := `.exit   Exit the repl
 .help   Print this help
 -----------------------
 Examples:
@@ -34,60 +34,69 @@ Examples:
 >>> print(os.date())
 >>> print(os.tmpname())
 >>> print(os.getenv("HOME"))
->>> print((function (x) return 2*x end)(3)) 
+>>> print((function (x) return 2*x end)(3))
 ----------------------`
-    fmt.Println(msg)
+	fmt.Println(msg)
+}
+
+func execute(L *C.lua_State, line string) bool {
+	expression := C.CString(strings.TrimSpace(line))
+	defer C.free(unsafe.Pointer(expression))
+
+	C.luaL_loadstring(L, expression)
+
+	if err := C.lua_pcallk(L, 0, 0, 0, 0, nil); err != 0 {
+		C.lua_settop(L, 0) // Clear stack
+		fmt.Println(line)
+		return false
+	}
+	result := C.lua_tolstring(L, (-1), nil)
+	defer C.free(unsafe.Pointer(result))
+
+	msg := C.GoString(result)
+	fmt.Print(msg)
+	return true
 }
 
 func repl(prompt string, in io.Reader, out io.Writer) {
-    scanner := bufio.NewScanner(in)
-    L := C.luaL_newstate()
-    C.luaL_openlibs(L)
+	scanner := bufio.NewScanner(in)
+	L := C.luaL_newstate()
+	C.luaL_openlibs(L)
 
-    for {
-        
-        fmt.Print(prompt)
-        
-        if !scanner.Scan() {
-            continue
-        }
-        
-        line := scanner.Text()
+	for {
 
-        if len(line) == 0 {
-            continue
-        }
-        
-        switch line {
-        case ".help":
-            help()
-            continue
-        case ".exit":
-            return
-        }
+		fmt.Print(prompt)
 
-        expression := C.CString(strings.TrimSpace(line))
-        C.luaL_loadstring(L, expression)
-        C.free(unsafe.Pointer(expression)) // Free pointer allocated with C.CString above
-        
-        if err := C.lua_pcallk(L, 0, 0, 0, 0, nil); err != 0 {
-            C.lua_settop(L, 0); // Clear stack
-            fmt.Println(line)
-            continue
-        }
-        result := C.lua_tolstring(L, (-1), nil)
-        msg := C.GoString(result)
-        C.free(unsafe.Pointer(result))  // Free pointer allocated with C.CString above
-        
-        fmt.Print(msg)
-    }
-    C.lua_close(L)
+		if !scanner.Scan() {
+			continue
+		}
+
+		line := scanner.Text()
+
+		if len(line) == 0 {
+			continue
+		}
+
+		switch line {
+		case ".help":
+			help()
+			continue
+		case ".exit":
+			return
+		}
+
+		if !execute(L, line) {
+			continue
+		}
+	}
+
+	C.lua_close(L)
 
 }
 
 func main() {
-   fmt.Println("Basic Lua REPL in Go!")
-   help()
-   prompt := ">>> "
-   repl(prompt, os.Stdin, os.Stdout)
+	fmt.Println("Basic Lua REPL in Go!")
+	help()
+	prompt := ">>> "
+	repl(prompt, os.Stdin, os.Stdout)
 }
